8acf5287c34fb8a29efd750954df495c24c5660e
hzy <748675131@qq.com>
2025-09-08 15:57:46 +0800

    test

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ app.get("/", (req, res) => {
   res.send("Hello Vercel1123!");
 });
 
+// 在 api/parse.js 中添加
+app.get('/parse-resource', (req, res) => {
+  res.json({ message: "接口可用，请使用 POST 方法调用" });
+});
+
 if (require.main === module) {
   const port = 3000;
   app.listen(port, () => {
